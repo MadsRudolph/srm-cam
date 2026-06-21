faa4d8ad609fb7084ad15bea5f735d930986f08f
Mads <68565302+MadsRudolph@users.noreply.github.com>
2026-06-21 21:31:03 +0200

    Add dataclass-to-Qt form builder

---
+++ gerber2rml/gui/form.py
@@ -0,0 +1,37 @@
+"""Build a Qt form from a dataclass instance; read edits back as a new instance."""
+from dataclasses import fields, replace
+from PySide6.QtWidgets import QWidget, QFormLayout, QDoubleSpinBox, QSpinBox, QCheckBox
+
+class DataclassForm(QWidget):
+    def __init__(self, instance, parent=None):
+        super().__init__(parent)
+        self._instance = instance
+        self._editors = {}
+        layout = QFormLayout(self)
+        for f in fields(instance):
+            val = getattr(instance, f.name)
+            if isinstance(val, bool):
+                w = QCheckBox(); w.setChecked(val)
+            elif isinstance(val, int):
+                w = QSpinBox(); w.setRange(-1, 100000); w.setValue(val)
+            else:  # float
+                w = QDoubleSpinBox(); w.setDecimals(3); w.setRange(-1000.0, 100000.0)
+                w.setSingleStep(0.1); w.setValue(float(val))
+            self._editors[f.name] = w
+            layout.addRow(f.name.replace("_", " "), w)
+
+    def _read(self, name):
+        w = self._editors[name]
+        if isinstance(w, QCheckBox):
+            return w.isChecked()
+        return w.value()
+
+    def set_field(self, name, value):
+        w = self._editors[name]
+        if isinstance(w, QCheckBox):
+            w.setChecked(bool(value))
+        else:
+            w.setValue(value)
+
+    def value(self):
+        return replace(self._instance, **{n: self._read(n) for n in self._editors})
---
+++ tests/test_form.py
@@ -0,0 +1,17 @@
+import os
+os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
+from PySide6.QtWidgets import QApplication
+from gerber2rml.config import TraceJob
+from gerber2rml.gui.form import DataclassForm
+
+_app = QApplication.instance() or QApplication([])
+
+def test_form_reads_dataclass_values():
+    form = DataclassForm(TraceJob())
+    assert form.value().bit_diameter == 0.4
+    assert form.value().offsets == 2
+
+def test_form_edit_reflects_in_value():
+    form = DataclassForm(TraceJob())
+    form.set_field("bit_diameter", 0.8)
+    assert form.value().bit_diameter == 0.8
